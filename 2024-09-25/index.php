cac6249bd9572f7e737728fe7fe60f5928c615d2
Savidya Perera <136046765+Savidya-Nirthana@users.noreply.github.com>
2024-09-25 09:55:27 +0530

    Add files via upload

---
+++ 2024-09-25/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        // echo "hellow";
+        // $car = array("Brand" => "Frod", "Model" => "Mustang", "Year"=>2022);
+        // foreach($car as $x => $y){
+        //     echo "<h1>$x : $y<h1>";
+        // }
+
+        // $authers = array("JK Rolling", "Wes Ball", "George R R Marting");
+        // $authers_slice = array_slice($authers, 1, 3);
+        
+        // foreach($authers_slice as $x){
+        //     echo "<p>$x<p>";
+        // }
+
+
+        // $myBook = array(
+        //     array("title"=> "The Grapes of Warth",
+        //     "author" => "Book 2 author",
+        //     "pubYear" => 1939),
+        //     array("title" => "Title of Book2",
+        //     "author" => "Book 2 author",
+        //     "pubYear" => 1939),
+        // );
+
+
+        // echo count(value: $myBook);
+
+        // $text = "apple,banana,orange,grape";
+        // $fruits = explode(",", $text);
+        // foreach($fruits as $x){
+        //     echo "<p>$x<p>";
+        // }
+
+
+        // $tt = implode(",",$authers);
+        // echo $tt;
+    ?>
+</body>
+</html>
